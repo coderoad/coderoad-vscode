0d67935ad0adbf1dde0c08e8bd4d839d93bb6dee
shmck <shawn.j.mckay@gmail.com>
2019-05-26 09:48:35 -0700

    add core typings

---
+++ src/typings/index.d.ts
@@ -0,0 +1,106 @@
+export interface TutorialLevel {
+    stageList: string[]
+    content?: {
+        title?: string
+        text?: string
+    }
+}
+
+export interface TutorialStage {
+    stepList: string[]
+    content?: {
+        title?: string
+        text?: string
+    }
+}
+
+export interface TutorialHint {
+    text: string
+    displayed?: boolean
+}
+
+export interface TutorialAction {
+    commits: string[]
+    commands?: string[]
+    files?: string[]
+}
+
+export interface TutorialStepContent {
+    text: string
+    title?: string
+}
+
+export interface TutorialStep {
+    content: TutorialStepContent
+    actions: {
+        setup: TutorialAction
+        solution: TutorialAction
+    }
+    hints?: TutorialHint[]
+}
+
+export interface TutorialData {
+    summary: TutorialSummary
+    levels: {
+        [levelId: string]: TutorialLevel
+    }
+    stages: {
+        [stageId: string]: TutorialStage
+    }
+    steps: {
+        [stepId: string]: TutorialStep
+    }
+}
+
+export interface TutorialMeta {
+    version: string
+    repo: string
+    createdBy: string
+    createdAt: string
+    updatedBy: string
+    updatedAt: string
+    contributors: string[]
+    languages: string[]
+    testRunner: string
+}
+
+export interface TutorialSummary {
+    title: string
+    description: string
+    levelList: string[]
+}
+
+export interface Tutorial {
+    id: string
+    meta: TutorialMeta
+    data: TutorialData
+}
+
+export interface Progress {
+    levels: {
+        [levelId: string]: boolean
+    }
+    stages: {
+        [stageId: string]: boolean
+    }
+    steps: {
+        [stepId: string]: boolean
+    }
+    complete: boolean
+}
+
+// current tutorial position
+export interface Position {
+    levelId: string
+    stageId: string
+    stepId: string
+    complete?: boolean
+}
+
+// current tutorial state
+
+export interface Action {
+    type: string
+    payload?: any
+    meta?: any
+}
