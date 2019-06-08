481807c47cf46fa72107a91e24a9a179615412ad
shmck <shawn.j.mckay@gmail.com>
2019-06-08 11:10:29 -0700

    run test action

--- src/editor/commands/index.ts
+++ src/editor/commands/index.ts
@@ -2,15 +2,15 @@ import * as vscode from 'vscode'
 import start from './start'
 import ReactPanel from '../views/createWebview'
 
-// import runTest from './runTest'
+import runTest from './runTest'
 // import loadSolution from './loadSolution'
 // import quit from './quit'
 
 const COMMANDS = {
   // TUTORIAL_SETUP: 'coderoad.tutorial_setup',
   START: 'coderoad.start',
-  OPEN_WEBVIEW: 'coderoad.open_webview'
-  // RUN_TEST: 'coderoad.test_run',
+  OPEN_WEBVIEW: 'coderoad.open_webview',
+  RUN_TEST: 'coderoad.test_run',
   // LOAD_SOLUTION: 'coderoad.solution_load',
   // QUIT: 'coderoad.quit',
 }
@@ -18,13 +18,15 @@ const COMMANDS = {
 
 export default (context: vscode.ExtensionContext): void => {
   const commands = {
-    [COMMANDS.START]: async function startCommand(): Promise<void> {
-      return start(context)
+    [COMMANDS.START]: () => {
+      start(context)
     },
     [COMMANDS.OPEN_WEBVIEW]: () => {
       ReactPanel.createOrShow(context.extensionPath);
     },
-    // [COMMANDS.RUN_TEST]: runTest,
+    [COMMANDS.RUN_TEST]: () => {
+      runTest()
+    },
     // [COMMANDS.LOAD_SOLUTION]: loadSolution,
     // [COMMANDS.QUIT]: () => quit(context.subscriptions),
   }
