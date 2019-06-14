23ab6b646751d4ea4fc27a70af15bf17c949b616
shmck <shawn.j.mckay@gmail.com>
2019-06-13 20:03:15 -0700

    fix complete button toggle

--- web-app/src/containers/Tutorial/StagePage.tsx
+++ web-app/src/containers/Tutorial/StagePage.tsx
@@ -25,7 +25,7 @@ const StagePage = (props: PageProps) => {
       ...data.steps[stepId],
       status: {
         // flag progressed steps as complete
-        complete: progress.steps[stepId] || false,
+        complete: progress.stages[stageId] || false,
         // set active step to active
         active: position.stepId === stepId,
       },
