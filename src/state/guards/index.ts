fc2c2174c3d8a9fef30a42e8f17857427ff5e1c1
shmck <shawn.j.mckay@gmail.com>
2019-07-21 21:40:29 -0700

    fix crash on steps complete

--- src/state/guards/index.ts
+++ src/state/guards/index.ts
@@ -5,21 +5,21 @@ export default {
     const { data, position, progress } = context
     const steps = data.stages[position.stageId].stepList
     // isn't final step yet
-    const hasNext = steps[steps.length - 1] !== position.stepId || !progress.stages[position.stageId]
+    const hasNext = !!position.stepId && (steps[steps.length - 1] !== position.stepId) || !progress.stages[position.stageId]
     console.log('GUARD: hasNextStep', hasNext)
     return hasNext
   },
   hasNextStage: (context: CR.MachineContext): boolean => {
     const { data, position } = context
     const stages = data.levels[position.levelId].stageList
-    const hasNext = stages[stages.length - 1] !== position.stageId
+    const hasNext = !!position.stageId && stages[stages.length - 1] !== position.stageId
     console.log('GUARD: hasNextStage', hasNext)
     return hasNext
   },
   hasNextLevel: (context: CR.MachineContext): boolean => {
     const { data, position } = context
     const levels = data.summary.levelList
-    const hasNext = levels[levels.length - 1] !== position.levelId
+    const hasNext = !!position.levelId && levels[levels.length - 1] !== position.levelId
     console.log('GUARD: hasNextLevel', hasNext)
     return hasNext
   },
