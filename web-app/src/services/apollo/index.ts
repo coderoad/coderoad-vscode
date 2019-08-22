39af55d36c0c71f70ff306e3ac42adf60e3063aa
shmck <shawn.j.mckay@gmail.com>
2019-08-21 21:11:28 -0700

    cleanup apollo server

--- web-app/src/services/apollo/index.ts
+++ web-app/src/services/apollo/index.ts
@@ -1,16 +1,16 @@
-import ApolloClient, { InMemoryCache } from 'apollo-boost'
+import ApolloClient, {InMemoryCache} from 'apollo-boost'
 
 import typeDefs from './typeDefs'
 import resolvers from './resolvers'
 
 const client = new ApolloClient({
-    uri: process.env.REACT_APP_GQL_URI,
-    headers: {
-        Authorization: process.env.GQL_AUTH_TOKEN,
-    },
-    cache: new InMemoryCache(),
-    typeDefs,
-    resolvers,
+	uri: process.env.REACT_APP_GQL_URI,
+	headers: {
+		Authorization: process.env.REACT_APP_GQL_AUTH_TOKEN,
+	},
+	cache: new InMemoryCache(),
+	typeDefs,
+	resolvers,
 })
 
 export default client
