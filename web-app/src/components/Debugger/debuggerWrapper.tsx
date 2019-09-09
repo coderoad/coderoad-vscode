619dd55acb023a8ae532451fc608249b294f6d7a
shmck <shawn.j.mckay@gmail.com>
2019-09-08 19:35:09 -0700

    fix debugger toggle

--- web-app/src/components/Debugger/debuggerWrapper.tsx
+++ web-app/src/components/Debugger/debuggerWrapper.tsx
@@ -2,15 +2,17 @@ import * as React from 'react'
 import Debugger from './index'
 import stateToString from './stateToString'
 
+const SHOW_DEBUGGER = JSON.parse(process.env.REACT_APP_DEBUG || 'false')
+
 const debuggerWrapper = (element: React.ReactElement, state: any) => {
-	if (process.env.REACT_APP_DEBUG) {
-		return (
-			<Debugger state={stateToString(state.value)} {...state.context}>
-				{element}
-			</Debugger>
-		)
-	}
-	return element
+  if (SHOW_DEBUGGER) {
+    return (
+      <Debugger state={stateToString(state.value)} {...state.context}>
+        {element}
+      </Debugger>
+    )
+  }
+  return element
 }
 
 export default debuggerWrapper
