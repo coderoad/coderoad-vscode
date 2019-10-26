ff84fa45ea2c9368bc2c8212f4241370f8ac58b5
shmck <shawn.j.mckay@gmail.com>
2019-10-26 14:38:45 -0700

    fix sidebar decorator overflow

--- web-app/stories/utils/SideBarDecorator.tsx
+++ web-app/stories/utils/SideBarDecorator.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 
 const styles = {
-  width: '20rem',
-  borderRight: '2px solid black',
-  height: window.innerHeight,
+  container: {
+    position: 'relative' as 'relative',
+    boxSizing: 'border-box' as 'border-box',
+    maxWidth: '20rem',
+    borderRight: '2px solid black',
+    width: '20rem',
+    height: window.innerHeight,
+  },
 }
 
-const SideBarDecorator = storyFn => <div style={styles}>{storyFn()}</div>
+const SideBarDecorator = storyFn => <div style={styles.container}>{storyFn()}</div>
 
 export default SideBarDecorator
