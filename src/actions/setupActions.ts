03e72163dab70b97bbe7d11cf2378b7ba00992ba
shmck <shawn.j.mckay@gmail.com>
2019-11-10 17:43:07 -0800

    remove unused

--- src/actions/setupActions.ts
+++ src/actions/setupActions.ts
@@ -4,34 +4,34 @@ import * as vscode from 'vscode'
 import * as git from '../services/git'
 import node from '../services/node'
 
-interface ErrorMessageFilter {
-	[lang: string]: {
-		[key: string]: string
-	}
-}
+// interface ErrorMessageFilter {
+// 	[lang: string]: {
+// 		[key: string]: string
+// 	}
+// }
 
 // TODO: should be loaded on startup based on language
-const commandErrorMessageFilter: ErrorMessageFilter = {
-	JAVASCRIPT: {
-		'node-gyp': 'Error running npm setup command'
-	}
-}
+// const commandErrorMessageFilter: ErrorMessageFilter = {
+// 	JAVASCRIPT: {
+// 		'node-gyp': 'Error running npm setup command'
+// 	}
+// }
 
 
 // TODO: pass command and command name down for filtering. Eg. JAVASCRIPT, 'npm install'
-const runCommands = async (commands: string[], language: string = 'JAVASCRIPT') => {
+const runCommands = async (commands: string[]) => {
 	for (const command of commands) {
 		const {stdout, stderr} = await node.exec(command)
 		if (stderr) {
 			console.error(stderr)
 			// language specific error messages from running commands
-			const filteredMessages = Object.keys(commandErrorMessageFilter[language])
-			for (const message of filteredMessages) {
-				if (stderr.match(message)) {
-					// ignored error
-					throw new Error('Error running setup command')
-				}
-			}
+			// const filteredMessages = Object.keys(commandErrorMessageFilter[language])
+			// for (const message of filteredMessages) {
+			// 	if (stderr.match(message)) {
+			// 		// ignored error
+			// 		throw new Error('Error running setup command')
+			// 	}
+			// }
 		}
 		console.log(`run command: ${command}`, stdout)
 	}
@@ -45,7 +45,8 @@ const disposeWatcher = (listener: string) => {
 	delete watchers[listener]
 }
 
-const setupActions = async (workspaceRoot: vscode.WorkspaceFolder, {commands, commits, files, listeners}: G.StepActions): Promise<void> => {
+const setupActions = async (workspaceRoot: vscode.WorkspaceFolder, actions: G.StepActions): Promise<void> => {
+	const {commands, commits, files, listeners} = actions
 	// run commits
 	if (commits) {
 		for (const commit of commits) {
@@ -54,31 +55,50 @@ const setupActions = async (workspaceRoot: vscode.WorkspaceFolder, {commands, co
 	}
 
 	// run file watchers (listeners)
-	if (listeners) {
-		for (const listener of listeners) {
-			if (!watchers[listener]) {
-				const pattern = new vscode.RelativePattern(
-					vscode.workspace.getWorkspaceFolder(workspaceRoot.uri)!,
-					listener
-				)
-				watchers[listener] = vscode.workspace.createFileSystemWatcher(
-					pattern
-				)
-				watchers[listener].onDidChange(() => {
-					// trigger save
-					vscode.commands.executeCommand('coderoad.run_test', null, () => {
-						// cleanup watcher on success
-						disposeWatcher(listener)
-					})
-				})
-			}
-		}
-	} else {
-		// remove all watchers
-		for (const listener of Object.keys(watchers)) {
-			disposeWatcher(listener)
-		}
-	}
+	// if (listeners) {
+	// 	console.log('listeners')
+	// 	for (const listener of listeners) {
+	// 		if (!watchers[listener]) {
+	// 			const pattern = new vscode.RelativePattern(
+	// 				vscode.workspace.getWorkspaceFolder(workspaceRoot.uri)!,
+	// 				listener
+	// 			)
+	// 			console.log(pattern)
+	// 			watchers[listener] = vscode.workspace.createFileSystemWatcher(
+	// 				pattern
+	// 			)
+	// 			watchers[listener].onDidChange(() => {
+	// 				console.log('onDidChange')
+	// 				// trigger save
+	// 				vscode.commands.executeCommand('coderoad.run_test', null, () => {
+	// 					// cleanup watcher on success
+	// 					disposeWatcher(listener)
+	// 				})
+	// 			})
+	// 			watchers[listener].onDidCreate(() => {
+	// 				console.log('onDidCreate')
+	// 				// trigger save
+	// 				vscode.commands.executeCommand('coderoad.run_test', null, () => {
+	// 					// cleanup watcher on success
+	// 					disposeWatcher(listener)
+	// 				})
+	// 			})
+	// 			watchers[listener].onDidDelete(() => {
+	// 				console.log('onDidDelete')
+	// 				// trigger save
+	// 				vscode.commands.executeCommand('coderoad.run_test', null, () => {
+	// 					// cleanup watcher on success
+	// 					disposeWatcher(listener)
+	// 				})
+	// 			})
+	// 		}
+	// 	}
+	// } else {
+	// 	// remove all watchers
+	// 	for (const listener of Object.keys(watchers)) {
+	// 		disposeWatcher(listener)
+	// 	}
+	// }
 
 	// run command
 	if (commands) {
