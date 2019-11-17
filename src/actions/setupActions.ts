99996685b8a439a2d28486232cb3a1308c6d317e
shmck <shawn.j.mckay@gmail.com>
2019-11-17 15:46:16 -0800

    setup git actions to send command feedback

--- src/actions/setupActions.ts
+++ src/actions/setupActions.ts
@@ -1,3 +1,4 @@
+import * as T from 'typings'
 import * as G from 'typings/graphql'
 import * as vscode from 'vscode'
 import * as git from '../services/git'
@@ -6,25 +7,38 @@ import node from '../services/node'
 import openFiles from './utils/openFiles'
 import loadWatchers from './utils/loadWatchers'
 
-const runCommands = async (commands: string[]) => {
+const runCommands = async (commands: string[], send: (action: T.Action) => void) => {
   if (!commands.length) {
     return
   }
   for (const command of commands) {
+    const process = {
+      title: command,
+      description: 'Running process',
+    }
+    send({ type: 'COMMAND_START', payload: { process: { ...process, status: 'RUNNING' } } })
     const { stdout, stderr } = await node.exec(command)
     if (stderr) {
+      // TODO: distinguish fail & error
       console.error(stderr)
+      send({ type: 'COMMAND_FAIL', payload: { process: { ...process, status: 'FAIL' } } })
+    } else {
+      send({ type: 'COMMAND_SUCCESS', payload: { process: { ...process, status: 'SUCCESS' } } })
     }
-    console.log(`run command: ${command}`, stdout)
   }
 }
 
-const setupActions = async (workspaceRoot: vscode.WorkspaceFolder, actions: G.StepActions): Promise<void> => {
+const setupActions = async (
+  workspaceRoot: vscode.WorkspaceFolder,
+  actions: G.StepActions,
+  send: (action: T.Action) => void, // send messages to client
+): Promise<void> => {
   const { commands, commits, files, watchers } = actions
 
   // 1. run commits
   if (commits) {
     for (const commit of commits) {
+      // TODO handle git errors
       await git.loadCommit(commit)
     }
   }
@@ -36,7 +50,7 @@ const setupActions = async (workspaceRoot: vscode.WorkspaceFolder, actions: G.St
   loadWatchers(watchers || [], workspaceRoot.uri)
 
   // 4. run command
-  await runCommands(commands || [])
+  await runCommands(commands || [], send)
 }
 
 export default setupActions
