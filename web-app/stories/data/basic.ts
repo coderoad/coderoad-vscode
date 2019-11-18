2d84ac6fe5471ba6957911e8acd2744e013ee0e3
shmck <shawn.j.mckay@gmail.com>
2019-11-17 20:57:06 -0800

    fix styles

--- web-app/stories/data/basic.ts
+++ web-app/stories/data/basic.ts
@@ -1,94 +1,94 @@
 import * as CR from 'typings'
 
 const basic: CR.Tutorial = {
-  id: 'tutorialId',
-  meta: {
-    version: '0.1.0',
-    repo: 'https://github.com/ShMcK/coderoad-vscode.git',
-    createdBy: 'shmck',
-    createdAt: 'Sat, 11 May 2019 18:25:24 GMT',
-    updatedBy: 'shmck',
-    updatedAt: 'Sat, 11 May 2019 18:25:24 GMT',
-    contributors: ['shmck'],
-    languages: ['javascript'],
-    testRunner: 'jest',
-  },
-  data: {
-    summary: {
-      title: 'Basic Test',
-      description: 'A basic coding skills example',
-      levelList: ['level1Id'],
-    },
-    levels: {
-      level1Id: {
-        stageList: ['stage1Id'],
-        content: {
-          title: 'Sum Level',
-          text: 'A description of this stage',
-        },
-      },
-    },
-    stages: {
-      stage1Id: {
-        stepList: ['step1Id', 'step2Id', 'step3Id'],
-        content: {
-          title: 'Sum Stage',
-          text: 'A description of this stage',
-        },
-      },
-    },
-    steps: {
-      step1Id: {
-        content: {
-          title: 'Sum',
-          text: 'Write a function that adds two numbers together',
-        },
-        actions: {
-          setup: {
-            commits: ['430500f', '8383061'],
-            commands: ['npm install'],
-            files: ['src/sum.js'],
-          },
-          solution: {
-            commits: ['abbe136'],
-          },
-        },
-        hints: [],
-      },
-      step2Id: {
-        content: {
-          title: 'Multiply',
-          text: 'Write a function that multiplies two numbers together',
-        },
-        actions: {
-          setup: {
-            commits: ['9cbb518'],
-            files: ['src/multiply.js'],
-          },
-          solution: {
-            commits: ['5ae011f'],
-          },
-        },
-        hints: [],
-      },
-      step3Id: {
-        content: {
-          title: 'Divide',
-          text: 'Write a function that divides',
-        },
-        actions: {
-          setup: {
-            commits: ['70c774c'],
-            files: ['src/divide.js'],
-          },
-          solution: {
-            commits: ['3180bed'],
-          },
-        },
-        hints: [],
-      },
-    },
-  },
+	id: 'tutorialId',
+	meta: {
+		version: '0.1.0',
+		repo: 'https://github.com/ShMcK/coderoad-vscode.git',
+		createdBy: 'shmck',
+		createdAt: 'Sat, 11 May 2019 18:25:24 GMT',
+		updatedBy: 'shmck',
+		updatedAt: 'Sat, 11 May 2019 18:25:24 GMT',
+		contributors: ['shmck'],
+		languages: ['javascript'],
+		testRunner: 'jest',
+	},
+	data: {
+		summary: {
+			title: 'Basic Test',
+			description: 'A basic coding skills example',
+			levelList: ['level1Id'],
+		},
+		levels: {
+			level1Id: {
+				stageList: ['stage1Id'],
+				content: {
+					title: 'Sum Level',
+					text: 'A description of this stage',
+				},
+			},
+		},
+		stages: {
+			stage1Id: {
+				stepList: ['step1Id', 'step2Id', 'step3Id'],
+				content: {
+					title: 'Sum Stage',
+					text: 'A description of this stage',
+				},
+			},
+		},
+		steps: {
+			step1Id: {
+				content: {
+					title: 'Sum',
+					text: 'Write a function that adds two numbers together',
+				},
+				actions: {
+					setup: {
+						commits: ['430500f', '8383061'],
+						commands: ['npm install'],
+						files: ['src/sum.js'],
+					},
+					solution: {
+						commits: ['abbe136'],
+					},
+				},
+				hints: [],
+			},
+			step2Id: {
+				content: {
+					title: 'Multiply',
+					text: 'Write a function that multiplies two numbers together',
+				},
+				actions: {
+					setup: {
+						commits: ['9cbb518'],
+						files: ['src/multiply.js'],
+					},
+					solution: {
+						commits: ['5ae011f'],
+					},
+				},
+				hints: [],
+			},
+			step3Id: {
+				content: {
+					title: 'Divide',
+					text: 'Write a function that divides',
+				},
+				actions: {
+					setup: {
+						commits: ['70c774c'],
+						files: ['src/divide.js'],
+					},
+					solution: {
+						commits: ['3180bed'],
+					},
+				},
+				hints: [],
+			},
+		},
+	},
 }
 
 export default basic
--- web-app/stories/utils/SideBarDecorator.tsx
+++ web-app/stories/utils/SideBarDecorator.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
 
 const styles = {
-  container: {
-    left: '25rem',
-    position: 'absolute' as 'absolute',
-    boxSizing: 'border-box' as 'border-box',
-    borderLeft: '2px solid black',
-    borderRight: '2px solid black',
-    width: '50rem',
-    height: window.innerHeight,
-  },
+	container: {
+		left: '25rem',
+		position: 'absolute' as 'absolute',
+		boxSizing: 'border-box' as 'border-box',
+		borderLeft: '2px solid black',
+		borderRight: '2px solid black',
+		width: '50rem',
+		height: window.innerHeight,
+	},
 }
 
 const SideBarDecorator = storyFn => <div style={styles.container}>{storyFn()}</div>
