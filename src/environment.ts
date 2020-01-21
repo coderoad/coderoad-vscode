0213633e97e34470e6e57d33487701de189b6d63
shmck <shawn.j.mckay@gmail.com>
2020-01-20 20:21:29 -0800

    setup Sentry

--- src/environment.ts
+++ src/environment.ts
@@ -3,10 +3,14 @@ require('dotenv').config({
 })
 
 interface Environment {
+  VERSION: string
+  NODE_ENV: string
   LOG: boolean
 }
 
 const environment: Environment = {
+  VERSION: process.env.VERSION || 'unknown',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   LOG: (process.env.LOG || '').toLowerCase() === 'true',
 }
 
---
+++ src/services/sentry/init.ts
@@ -0,0 +1,7 @@
+import * as sentry from '@sentry/node'
+import environment from '../../environment'
+
+sentry.init({
+  dsn: 'https://df4a6ae19e8b44ed9a87ae4432dab9df@sentry.io/1889368',
+  environment: environment.NODE_ENV,
+})
