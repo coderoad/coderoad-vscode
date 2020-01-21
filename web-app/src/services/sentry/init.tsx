771985020537107471d06f6c2ccdd030b6ea5f06
shmck <shawn.j.mckay@gmail.com>
2020-01-20 21:36:17 -0800

    handle sentry init on launch in web

--- web-app/src/services/sentry/init.tsx
+++ web-app/src/services/sentry/init.tsx
@@ -1,7 +1,12 @@
 import * as sentry from '@sentry/browser'
 import { NODE_ENV } from '../../environment'
 
-sentry.init({
-  dsn: 'https://701cee76c32a4408b2fcb6af3e139d46@sentry.io/1889371',
-  environment: NODE_ENV,
-})
+try {
+  sentry.init({
+    dsn: 'https://701cee76c32a4408b2fcb6af3e139d46@sentry.io/1889371',
+    environment: NODE_ENV,
+  })
+} catch (error) {
+  console.log('Error in Sentry init')
+  console.log(error)
+}
