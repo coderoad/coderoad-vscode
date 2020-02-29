15b542d7a6a65415a0d998fd3fdf2857d7437ce5
shmck <shawn.j.mckay@gmail.com>
2020-02-29 15:29:57 -0800

    fix parser

--- src/services/testRunner/parser.test.ts
+++ src/services/testRunner/parser.test.ts
@@ -39,4 +39,32 @@ not ok 4 - Second to fail
 `
     expect(parser(example).message).toBe('First to fail')
   })
+
+  test('should parse mocha tap example', () => {
+    const example = `
+1..3
+ok 1 itemList data should not be changed
+ok 2 sumItems shouldn't return NaN
+ok 3 sumItems should total numbers accurately
+# tests 3
+# pass 3
+# fail 0
+# skip 0
+`
+    expect(parser(example).ok).toBe(true)
+  })
+
+  test('should return failure message for mocha tap example', () => {
+    const example = `
+1..3
+ok 1 itemList data should not be changed
+not ok 2 sumItems shouldn't return NaN
+ok 3 sumItems should total numbers accurately
+# tests 3
+# pass 2
+# fail 1
+# skip 0
+`
+    expect(parser(example).message).toBe("sumItems shouldn't return NaN")
+  })
 })
