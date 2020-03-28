a24adcaddce6d8a296fef7f98b8941fdb695fa4f
shmck <shawn.j.mckay@gmail.com>
2020-03-28 16:35:54 -0700

    use single .env

--- src/environment.ts
+++ src/environment.ts
@@ -1,17 +1,19 @@
 require('dotenv').config({
-  path: './.env',
+  path: './web-app/.env',
 })
 
 interface Environment {
   VERSION: string
   NODE_ENV: string
   LOG: boolean
+  API_URL: string
 }
 
 const environment: Environment = {
   VERSION: process.env.VERSION || 'unknown',
   NODE_ENV: process.env.NODE_ENV || 'production',
   LOG: (process.env.LOG || '').toLowerCase() === 'true',
+  API_URL: process.env.REACT_APP_GQL_URI || '',
 }
 
 export default environment
--- web-app/src/environment.ts
+++ web-app/src/environment.ts
@@ -1,13 +1,12 @@
 // validate .env
-// const requiredKeys = ['REACT_APP_GQL_URI']
-// for (const required of requiredKeys) {
-//   if (!process.env[required]) {
-//     throw new Error(`Missing Environmental Variables: ${required}`)
-//   }
-// }
+const requiredKeys = ['REACT_APP_GQL_URI']
+for (const required of requiredKeys) {
+  if (!process.env[required]) {
+    throw new Error(`Missing Environmental Variables: ${required}`)
+  }
+}
 
-export const GQL_URI: string =
-  process.env.REACT_APP_GQL_URI || 'https://33mf420q4m.execute-api.us-west-2.amazonaws.com/stage/api-stage'
+export const GQL_URI: string = process.env.REACT_APP_GQL_URI || 'NO API URI PROVIDED'
 export const DEBUG: boolean = (process.env.REACT_APP_DEBUG || '').toLowerCase() === 'true'
 export const VERSION: string = process.env.VERSION || 'unknown'
 export const NODE_ENV: string = process.env.NODE_ENV || 'production'
