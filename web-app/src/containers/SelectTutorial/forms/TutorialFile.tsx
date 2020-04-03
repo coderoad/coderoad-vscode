a1f554b810c657dcce4d481960b38bda872184d4
shmck <shawn.j.mckay@gmail.com>
2020-04-02 20:36:11 -0700

    fix default chrome bug with upload file

--- web-app/src/containers/SelectTutorial/forms/TutorialFile.tsx
+++ web-app/src/containers/SelectTutorial/forms/TutorialFile.tsx
@@ -8,8 +8,20 @@ interface Props {
   onLoadSummary(data: TT.Tutorial): void
 }
 
+const styles = {
+  uploadFileButton: {
+    padding: '0.3rem 0.5rem',
+    outline: '1px dotted rgb(51, 51, 51)',
+    borderRadius: '0.2rem',
+    fontWeight: 400,
+    fontFamily:
+      '-apple-system, system-ui, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;',
+  },
+}
+
 const TutorialFile = (props: Props) => {
   const onChange = (evt: any) => {
+    evt.preventDefault()
     const files = evt.target.files
 
     if (!files.length) {
@@ -29,7 +41,12 @@ const TutorialFile = (props: Props) => {
   return (
     <Form style={{ maxWidth: '600px' }}>
       <FormItem label="Load coderoad config.json">
-        <input type="file" accept="application/json" onChange={onChange} />
+        <br />
+        <label style={styles.uploadFileButton}>
+          Upload
+          <input style={{ display: 'none' }} type="file" accept="application/json" onChange={onChange} />
+        </label>
+        <br />
       </FormItem>
       <br />
     </Form>
