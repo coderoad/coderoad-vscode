fdf68c206bba113d18558368c8f946cf66242efb
shmck <shawn.j.mckay@gmail.com>
2020-04-04 09:48:52 -0700

    refactor parser to capture multiple failures

--- src/services/testRunner/parser.test.ts
+++ src/services/testRunner/parser.test.ts
@@ -1,22 +1,20 @@
 import parser from './parser'
 
 describe('parser', () => {
-  test('should detect success', () => {
+  test('should pass single success', () => {
+    const example = `
+1..1
+ok 1 - Should pass
+`
+    expect(parser(example)).toEqual({ ok: true, fails: [] })
+  })
+  test('should detect multiple successes', () => {
     const example = `
 1..2
 ok 1 - Should pass
 ok 2 - Should also pass
 `
-    expect(parser(example)).toEqual({ ok: true })
-  })
-  test('should detect failure', () => {
-    const example = `
-1..3
-ok 1 - Should pass
-not ok 2 - This one fails
-ok 3 - Also passes
-`
-    expect(parser(example).ok).toBe(false)
+    expect(parser(example)).toEqual({ ok: true, fails: [] })
   })
   test('should detect failure if no tests passed', () => {
     const example = `
@@ -29,6 +27,15 @@ not ok 1 ● sum › should add two numbers together
 `
     expect(parser(example).ok).toBe(false)
   })
+  test('should detect single failure among successes', () => {
+    const example = `
+1..3
+ok 1 - Should pass
+not ok 2 - This one fails
+ok 3 - Also passes
+`
+    expect(parser(example).ok).toBe(false)
+  })
   test('should return failure message', () => {
     const example = `
 1..4
@@ -37,7 +44,7 @@ not ok 2 - First to fail
 ok 3 - Also passes
 not ok 4 - Second to fail
 `
-    expect(parser(example).message).toBe('First to fail')
+    expect(parser(example).fails).toEqual([{ message: 'First to fail' }, { message: 'Second to fail' }])
   })
 
   test('should parse mocha tap example', () => {
@@ -65,6 +72,6 @@ ok 3 sumItems should total numbers accurately
 # fail 1
 # skip 0
 `
-    expect(parser(example).message).toBe("sumItems shouldn't return NaN")
+    expect(parser(example).fails).toEqual([{ message: "sumItems shouldn't return NaN" }])
   })
 })
--- src/services/testRunner/parser.ts
+++ src/services/testRunner/parser.ts
@@ -1,29 +1,69 @@
 interface ParserOutput {
   ok: boolean
-  message?: string
+  fails: Array<{ message: string; details?: string }>
 }
 
-const fail = /^not ok \d+\s(\-\s)?(.+)+$/
-const ok = /^ok/
+const r = {
+  fail: /^not ok \d+\s(\-\s)?(.+)+$/,
+  pass: /^ok/,
+  details: /^#\s{2}(.+)$/,
+}
+
+const detect = (type: 'fail' | 'pass' | 'details', text: string) => r[type].exec(text)
 
 const parser = (text: string): ParserOutput => {
   const lines = text.split('\n')
-  let hasPass = false
+
+  const result: ParserOutput = {
+    ok: false,
+    fails: [],
+  }
+
+  // temporary holder of error detail strings
+  let currentDetails: string | null = null
+
+  const addCurrentDetails = () => {
+    if (currentDetails) {
+      result.fails[result.fails.length - 1].details = currentDetails
+      currentDetails = null
+    }
+  }
+
   for (const line of lines) {
-    if (line.length) {
-      // parse failed test
-      const failRegex = fail.exec(line)
-      if (!!failRegex) {
-        return { ok: false, message: failRegex[2] }
+    if (!line.length) {
+      continue
+    }
+    // be optimistic! check for success
+    if (!result.ok && !result.fails.length) {
+      if (!!detect('pass', line)) {
+        result.ok = true
+        addCurrentDetails()
+        continue
       }
-      if (!hasPass) {
-        if (!!ok.exec(line)) {
-          hasPass = true
-        }
+    }
+
+    // check for failure
+    const isFail = detect('fail', line)
+    if (!!isFail) {
+      result.ok = false
+      result.fails.push({ message: isFail[2] })
+      addCurrentDetails()
+      continue
+    }
+
+    // check for error details
+    const isDetails = detect('details', line)
+    if (!!isDetails) {
+      const lineDetails: string = isDetails[2]
+      if (!currentDetails) {
+        currentDetails = lineDetails
+      } else {
+        // @ts-ignore ignore as it must be a string
+        currentDetails += `\n${lineDetails}`
       }
     }
   }
-  return { ok: hasPass }
+  return result
 }
 
 export default parser
