2320b29f6f3749bc631e75db6058c8648e0ecb42
argemiront <argemiront@gmail.com>
2020-04-12 21:05:07 -0700

    add an option to build on windows
    Signed-off-by: argemiront <argemiront@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,7 +14,16 @@ tsc -p ./
 cd web-app
 npm run build
 cd ..
+
+# For Windows build: switch the next 2 lines
+if [[ "$OSTYPE" == "msys" ]]; then
+echo "linux subsystem on windows selected"
+cp -R ./web-app/build/ ./
+else
+echo "Unix system selected"
 cp -R ./web-app/build/ ./build/
+fi
+
 node scripts/fixFontPaths.js
 
 echo "Build complete!"
