18cbb40876823d50f37203f173b9d7480f2362ca
shmck <shawn.j.mckay@gmail.com>
2020-04-18 10:22:01 -0700

    sentry only in production
    Signed-off-by: shmck <shawn.j.mckay@gmail.com>

--- web-app/src/services/sentry/onError.ts
+++ web-app/src/services/sentry/onError.ts
@@ -1,19 +1,21 @@
 import * as sentry from '@sentry/browser'
 // import { Scope } from '@sentry/hub'
-import { VERSION } from '../../environment'
+import { VERSION, NODE_ENV } from '../../environment'
 
 const onError = (error: Error) => {
-  // set user scope https://docs.sentry.io/enriching-error-data/scopes/?platform=node
-  sentry.withScope((scope: any) => {
-    scope.setTag('VERSION', VERSION)
-    // if (user) {
-    //   scope.setUser({
-    //     id: user.id,
-    //     email: user.email || 'unknown',
-    //   })
-    // }
-    sentry.captureException(error)
-  })
+  if (NODE_ENV === 'production') {
+    // set user scope https://docs.sentry.io/enriching-error-data/scopes/?platform=node
+    sentry.withScope((scope: any) => {
+      scope.setTag('VERSION', VERSION)
+      // if (user) {
+      //   scope.setUser({
+      //     id: user.id,
+      //     email: user.email || 'unknown',
+      //   })
+      // }
+      sentry.captureException(error)
+    })
+  }
 }
 
 export default onError
