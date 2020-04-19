ab397a4787a28ba0daec3010f03c6e85b0e9325a
shmck <shawn.j.mckay@gmail.com>
2020-04-18 19:38:15 -0700

    load tutorials from yaml
    Signed-off-by: shmck <shawn.j.mckay@gmail.com>

--- web-app/src/services/hooks/useFetch.ts
+++ web-app/src/services/hooks/useFetch.ts
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import * as yaml from 'js-yaml'
 
 const useFetch = <T>(url: string, options: object = {}): { data: T | null; error: string | null; loading: boolean } => {
   const [data, setData] = React.useState(null)
@@ -8,9 +9,24 @@ const useFetch = <T>(url: string, options: object = {}): { data: T | null; error
     const fetchData = async () => {
       try {
         const res = await fetch(url, options)
+        // sometimes returns longer content-types such as "text/plain; charset=utf-8"
+        const contentType = res.headers.get('content-type')?.split(';')[0]
+        let data
+        switch (contentType) {
+          case 'application/json':
+            data = await res.json()
+            break
+          case 'text/plain':
+          case 'text/yaml':
+            const text = await res.text()
+            data = yaml.safeLoad(text)
+            break
+          default:
+            throw new Error(`Unsupported content-type "${contentType}"`)
+        }
+
         setLoading(false)
-        const json = await res.json()
-        setData(json)
+        setData(data)
       } catch (error) {
         setError(error)
       }
