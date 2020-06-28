947d2fd543bfda95c9217e050a2c526b0c387360
shmck <shawn.j.mckay@gmail.com>
2020-06-28 16:29:38 -0700

    cleanup eslint config for web-app
    Signed-off-by: shmck <shawn.j.mckay@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   plugins: ['prettier'],
   extends: [
+    'react-app',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended',
