696b27fd00c9391456ee0828b363cdae54da555a
Shubham <shubhamshahrising@gmail.com>
2020-08-19 11:50:33 +0530

    Refactor to new file
    Signed-off-by: Shubham <shubhamshahrising@gmail.com>

--- web-app/src/containers/Tutorial/components/ProgressPie.tsx
+++ web-app/src/containers/Tutorial/components/ProgressPie.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Progress, Icon } from '@alifd/next'
+import useNaturalProgress from 'services/hooks/useNaturalProgress'
 
 interface Props {
   current: number
@@ -7,23 +8,7 @@ interface Props {
 }
 
 const ProgressPie = (props: Props) => {
-  const [progress, setProgress] = React.useState(0)
-  React.useEffect(() => {
-    let timeout: any
-    let difference = (props.current - progress) / 4
-    // for difference>0.01 update progress or make it stop
-    let newProgress = difference > 0.01 ? progress + difference : props.current
-    if (progress < props.current) {
-      timeout = setTimeout(() => {
-        setProgress(newProgress)
-      }, 100)
-    }
-    return () => {
-      if (timeout) {
-        clearTimeout(timeout)
-      }
-    }
-  }, [progress])
+  const progress = useNaturalProgress({ stop: props.current })
 
   const progressPercent = Math.floor((progress / props.max) * 100)
 
