a7673e4243a4952b0b1dede8688e23776d8b9b63
shmck <shawn.j.mckay@gmail.com>
2021-06-05 19:17:04 -0700

    eslint update
    Signed-off-by: shmck <shawn.j.mckay@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,15 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   plugins: ['prettier'],
-  extends: [
-    'react-app',
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended',
-  ],
+  extends: ['react-app', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
     // 'react/forbid-component-props': [1, { forbid: ['style'] }],
     // 'react/forbid-dom-props': [1, { forbid: ['style'] }],
+    'space-before-function-paren': 0,
+    'prettier/prettier': 'error',
   },
 }
