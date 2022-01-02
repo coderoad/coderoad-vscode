020b0aeeadf1b08407198bb2593f08349856d4c5
shmck <shawn.j.mckay@gmail.com>
2022-01-02 12:08:14 -0800

    only log non-trusted security warnings
    Signed-off-by: shmck <shawn.j.mckay@gmail.com>

--- web-app/src/services/listeners/index.ts
+++ web-app/src/services/listeners/index.ts
@@ -1,9 +1,8 @@
-import ReactDOM from 'react-dom'
 import logger from '../logger'
-
-// document listeners
 
 document.addEventListener('securitypolicyviolation', (event) => {
   // TODO: add error handling
-  logger(`Security warning for resource: ${JSON.stringify(event)}`)
+  if (event.isTrusted !== true) {
+    logger(`Security warning for resource: ${JSON.stringify(event)}`)
+  }
 })
